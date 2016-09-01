2a99cd12b1c39270f705c990c28ed357d2c23b59
Doremiska <doremiska@gmail.com>
2016-09-01 15:58:39 +0200

    Ajout/Edit/Delete fonctionnel (avant ajout du BundleUpload)

--- src/AdminBundle/Controller/AdminController.php
+++ src/AdminBundle/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use AdminBundle\Form\AdvertType;
 use WebsiteBundle\Entity\Advert;
 
@@ -26,19 +27,58 @@ class AdminController extends Controller
             
             $request->getSession()->getFlashBag()->add('notice', "Annonce bien enregistrée.");
             
-            return $this->redirectToRoute('website_homepage');
+            return $this->redirect($this->generateUrl('website_homepage').'#actualites');
         }
         
         return $this->render('AdminBundle:Admin:add.html.twig', array('form' => $form->createView()));
     }
     
-    public function editAction()
+    public function editAction($id, Request $request)
     {
-        return $this->render('AdminBundle:Admin:edit.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $advert = $em->getRepository('WebsiteBundle:Advert')->find($id);
+        
+        if ($advert === null) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+        
+        $form = $this->createForm(AdvertType::class, $advert);
+        
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em->flush();
+            
+            $request->getSession()->getFlashBag()->add('notice', "L'annonce a bien été modifiée.");
+            
+            return $this->redirect($this->generateUrl('website_homepage').'#actualites');
+        }
+        
+        return $this->render('AdminBundle:Admin:edit.html.twig', array('form' => $form->createView()));
     }
     
-    public function deleteAction()
+    public function deleteAction($id, Request $request)
     {
-        return $this->render('AdminBundle:Admin:delete.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $advert = $em->getRepository('WebsiteBundle:Advert')->find($id);
+        
+        if ($advert === null) {
+            throw new NotFoundHttpException("L'annonce d'id ".$id." n'existe pas.");
+        }
+        
+        // On crée un formulaire vide qui ne contiendra que le champ CSRF (cela permet de protéger la suppression d'annonce contre cette faille)
+        $form = $this->get('form.factory')->create();
+        
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em->remove($advert);
+            $em->flush();
+            
+            $request->getSession()->getFlashBag()->add('notice', "L'annonce a bien été supprimée.");
+            
+            return $this->redirect($this->generateUrl('website_homepage').'#actualites');
+        }
+        
+        return $this->render('AdminBundle:Admin:delete.html.twig', array(
+            'advert' => $advert,
+            'form' => $form->createView()
+        ));
     }
 }
